3f122d47ddc354d78712e4eabc9fdd724c9ce510
Kalie <kaliecalnan@gmail.com>
2025-08-12 16:18:33 -0400

    Complete Exercise 09 - sumAll
    Use Javascript to take every integer between 2 integers and get the sum.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    // Check if a or b is negative, or not an integer
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    let largerNumber = 0;
+    let smallerNumber = 0;
+
+    if (a > b) {
+        largerNumber = a;
+        smallerNumber = b;
+    } else {
+        largerNumber = b;
+        smallerNumber = a;
+    }
+
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

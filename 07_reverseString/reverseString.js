4f7c98e90377790ecf1aac0c304f4495ac9e3419
Kalie <kaliecalnan@gmail.com>
2025-08-12 10:21:08 -0400

    Complete Exercise 07 - reverseString
    Use Javascript to reverse a string.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
 const reverseString = function(string) {
+    let array = string.split("");
+    let length = string.length;
+    let reverse = Math.floor(length / 2);
 
+    for (let i = 0; i < reverse; i++) {
+        let temp = array[i];
+        array[i] = array[length - i - 1];
+        array[length - i - 1] = temp;
+    }
+
+    return array.join("");
 }
 
 // Do not edit below this line

e7900777bb0f029a49f6f67012e3189421247a78
Kalie <kaliecalnan@gmail.com>
2025-08-12 11:38:32 -0400

    Complete Exercise 08 - removeFromArray
    Use Javascript to remove elements of a certain value from an array.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    for (let i = 0; i < remove.length; i++) {
+        while (array.indexOf(remove[i]) !== -1) {
+            array.splice(array.indexOf(remove[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
